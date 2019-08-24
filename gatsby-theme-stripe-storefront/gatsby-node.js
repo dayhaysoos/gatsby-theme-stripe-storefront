354e71bc0093c28bf09344b1d63cc558ba76d0af
Jason Lengstorf <jason@lengstorf.com>
2019-08-24 08:39:24 -0700

    fix: get env vars working, switch to site dev

--- gatsby-theme-stripe-storefront/gatsby-node.js
+++ gatsby-theme-stripe-storefront/gatsby-node.js
@@ -1,124 +1,125 @@
-require("dotenv").config()
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
-const axios = require("axios")
+const axios = require('axios');
 
 // grab sku data from stripe api
 
 exports.sourceNodes = async ({
-    actions,
-    createNodeId,
-    createContentDigest,
-    reporter,
+  actions,
+  createNodeId,
+  createContentDigest,
+  reporter
 }) => {
-    const result = await axios({
-        method: "GET",
-        url: "https://api.stripe.com/v1/skus",
-        headers: {
-            Authorization: `Bearer ${process.env.STRIPE_API_SECRET}`,
-        },
-    })
-
-    if (result.errors) {
-        reporter.panic("Error loading skus", JSON.stringify(result.errors))
+  const result = await axios({
+    method: 'GET',
+    url: 'https://api.stripe.com/v1/skus',
+    headers: {
+      Authorization: `Bearer ${process.env.STRIPE_API_SECRET}`
     }
+  });
 
-    const skuList = result.data
+  if (result.errors) {
+    reporter.panic('Error loading skus', JSON.stringify(result.errors));
+  }
 
-    // format sku data to something more desirable
-    // create nodeId in the process
-    // create type in internal Object
+  const skuList = result.data;
 
-    skuList.data.forEach(sku => {
-        const node = {
-            ...sku,
-            id: createNodeId(`Stripe-${sku.id}`),
-            name: sku.attributes.name,
-            slug: sku.attributes.name,
-            image: sku.image ? sku.image : 'no-image',
-            internal: {
-                type: "StripeSku",
-                contentDigest: createContentDigest(sku),
-            },
-        }
+  // format sku data to something more desirable
+  // create nodeId in the process
+  // create type in internal Object
 
-        // create node with processed data
+  skuList.data.forEach(sku => {
+    const node = {
+      ...sku,
+      id: createNodeId(`Stripe-${sku.id}`),
+      name: sku.attributes.name,
+      slug: sku.attributes.name,
+      image: sku.image ? sku.image : 'no-image',
+      internal: {
+        type: 'StripeSku',
+        contentDigest: createContentDigest(sku)
+      }
+    };
 
-        actions.createNode(node)
-    })
-}
+    // create node with processed data
+
+    actions.createNode(node);
+  });
+};
 
 // create url slug for each product
 exports.createResolvers = ({ createResolvers }, options) => {
-    const basePath = options.basePath || '/';
+  const basePath = options.basePath || '/';
 
-    // Quick-and-dirty helper to convert strings into URL-friendly slugs.
-    const slugify = str => {
-        const slug = str
-            .toLowerCase()
-            .replace(/[^a-z0-9]+/g, '-')
-            .replace(/(^-|-$)+/g, '');
+  // Quick-and-dirty helper to convert strings into URL-friendly slugs.
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)+/g, '');
 
-        return `/${basePath}/${slug}`.replace(/\/\/+/g, '/');
-    };
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, '/');
+  };
 
-    const formatPrice = num => {
-        const price = (num / 100).toFixed(2)
-        return price;
+  const formatPrice = num => {
+    const price = (num / 100).toFixed(2);
+    return price;
+  };
+
+  // reformat "slug" value in this resolver
+  createResolvers({
+    StripeSku: {
+      slug: {
+        resolve: source => slugify(source.name)
+      },
+      price: {
+        resolve: source => formatPrice(source.price)
+      }
     }
-
-    // reformat "slug" value in this resolver
-    createResolvers({
-        StripeSku: {
-            slug: {
-                resolve: source => slugify(source.name)
-            },
-            price: {
-                resolve: source => formatPrice(source.price)
-            }
-        }
-    });
+  });
 };
-
 
 // create pages for each item
 
 exports.createPages = async ({ actions, graphql, reporter }, options) => {
-    const basePath = options.basePath || '/';
+  const basePath = options.basePath || '/';
+
+  actions.createPage({
+    path: basePath,
+    component: require.resolve('./src/templates/skus.js')
+  });
+
+  const result = await graphql(`
+    query {
+      allStripeSku {
+        nodes {
+          name
+          price
+          currency
+          slug
+          image
+        }
+      }
+    }
+  `);
+
+  if (result.erros) {
+    reporter.panic('error loading products', reporter.errors);
+  }
+
+  const skus = result.data.allStripeSku.nodes;
+
+  skus.forEach(sku => {
+    const slug = sku.slug;
 
     actions.createPage({
-        path: basePath,
-        component: require.resolve('./src/templates/skus.js')
+      path: slug,
+      component: require.resolve('./src/templates/sku.js'),
+      context: {
+        skuID: sku.id
+      }
     });
-
-    const result = await graphql(`
-    query {
-        allStripeSku {
-            nodes {
-              name
-              price
-              currency
-              slug
-              image
-            }
-          }
-    }
-    `)
-
-    if(result.erros) {
-        reporter.panic('error loading products', reporter.errors);
-    }
-
-    const skus = result.data.allStripeSku.nodes;
-    
-    skus.forEach(sku => {
-        const slug = sku.slug;
-
-        actions.createPage({
-            path: slug,
-            component: require.resolve('./src/templates/sku.js'),
-            context: {
-                skuID: sku.id
-            }
-        })
-    })
-}
+  });
+};
