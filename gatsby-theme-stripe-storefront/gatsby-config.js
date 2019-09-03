2f5f9bcc9fe9d57ab820585e6710b7fc5990d28e
dayhaysoos <ndejesus1227@gmail.com>
2019-09-03 10:29:52 -0400

    added plugin for pages

--- gatsby-theme-stripe-storefront/gatsby-config.js
+++ gatsby-theme-stripe-storefront/gatsby-config.js
@@ -5,5 +5,11 @@ module.exports = {
   plugins: [
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-stripe',
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
   ],
 }
